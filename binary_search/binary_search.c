2e3385ef5aa834655c626cee0e91fff7340544da
shovdmi <shovdmi@yandex.ru>
2020-07-15 21:06:40 +0300

    small changes

--- binary_search/binary_search.c
+++ binary_search/binary_search.c
@@ -1,30 +1,32 @@
-typedef int (element_reader_t)(void* element, size_t index); // returns -1 on error
-typedef int (comparer_tape_t)(void* value1,void* value2);
+#include <stddef.h>
+#include "binary_search.h"
 
-ptrdiff_t leftmost_binary_search(size_t index_left, size_t index_right, void *value, comparer_t *compare, element_reader_t *read_element, void* element_ptr)
-{	
+ptrdiff_t leftmost_binary_search(size_t index_left, size_t index_right, void *value, comparer_t *compare, element_reader_t *read_element, void *element_ptr)
+{
 	ptrdiff_t index_diff;
-	while(index_right > index_left) {
+	while(index_right > index_left)
+	{
 		size_t index_middle = (index_left + index_right) / 2; //  m = l + diff/2 --> m = (l + r)/2 = (l + (l + diff))/2 = (2*l + diff)/2 = l + diff/2
-		if ((*read_element)(element_ptr, index_middle)) {     // Error during reading
-			return -1;
+		if ((*read_element)(element_ptr, index_middle) != 0)
+		{     // Error during reading means "record is empty"
+			index_right = index_middle;                   //  r = m;
+			continue;
 		}
-		if ((*compare)(element_ptr, value) < 0) {             //  arr[m] < val ?
+		if ((*compare)(element_ptr, value) < 0)             //  arr[m] < val ?
+		{
 			index_left = index_middle + 1;                //  l = m + 1;
-		} else {
+		}
+		else
+		{
 			index_right = index_middle;                   //  r = m;
 		}
 	}
-	
-	if ((*read_element)(element_ptr, index_left)) // Error during reading			
+
+	/*if ((*compare)(element_ptr, value) != 0)
+	{
 		return -1;
-	}
-	if ((*compare)(element_ptr, value) != 0) {		
-		return -1;
-	}
- 
+	}*/
+
 	return index_left;                                            // return l
 }
 
-
-// TODO: usage : if ((index_left >=0) && (index_left < array_size)) { ...
