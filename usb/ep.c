920226f59b75e39439e96f6291317b7c5abba2c2
Dmitry <shovdmi@yandex.ru>
2020-08-18 09:31:41 +0300

    Renamed functions according to what they actually do

--- usb/ep.c
+++ usb/ep.c
@@ -1,18 +1,38 @@
 #include "ep.h"
 
-inline uint16_t ep_get_value(uint8_t ep_number)
+inline uint16_t ep_read_value(uint8_t ep_number)
 {
 	volatile uint32_t *ep_ptr = (uint32_t*)&USB->EP0R;
 	uint16_t ep_value = ep_ptr[ep_number];
 	return ep_value;
 }
 
-inline void ep_set_value(uint8_t ep_number, uint16_t value)
+inline void ep_write_value(uint8_t ep_number, uint16_t value)
 {
 	volatile uint32_t *ep_ptr = (uint32_t*)&USB->EP0R;
+	// TODO: is there 32-bit access either on F103 and on L053??
 	ep_ptr[ep_number] = (uint32_t)value;
 }
 
+#if 0
+inline void ep_set_zero(uint8_t ep_number)
+{
+	volatile uint32_t *ep_ptr = (uint32_t*)&USB->EP0R;
+	// TODO: is there 32-bit access either on F103 and on L053??
+	ep_ptr[ep_number] = (uint32_t)0;       // write zero to w0-bits and rw-bits
+	ep_ptr[ep_number] = ep_ptr[ep_number]; // write zero to t-bits, as x ^ x == 0
+}
+
+// DO NOT USE this if you calculate ep_value using functions defined down below.
+// USE ep_write_value() instead!
+inline void ep_set_value(uint8_t ep_number, uint16_t value)
+{
+	assert(false);
+
+	ep_make_zero(ep_number);
+	ep_write_value(ep_number, value);
+}
+#endif
 
 
 // here we assume that user calls using the value of Endpoint register as a parameter
