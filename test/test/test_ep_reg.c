b080fa9a23548d2ae495b62faf14b4bbd66bc90a
Dmitry <shovdmi@yandex.ru>
2020-09-07 19:08:00 +0300

    Added Set/Clear writable bits

--- test/test/test_ep_reg.c
+++ test/test/test_ep_reg.c
@@ -25,6 +25,20 @@ uint8_t w[8] = {
         0,  0,  1,  0,  0,  1,  1,  1,      // normal-write bits
 };
 
+uint8_t w_set_tbl[8] = {
+// v |  0 | 0 | 0 | 0 | 1 | 1 | 1 | 1  |    (value to be written)
+// r |  0 | 0 | 1 | 1 | 0 | 0 | 1 | 1  |    (register value)
+// m |  0 | 1 | 0 | 1 | 0 | 1 | 0 | 1  |    (mask)
+        0,  0,  1,  1,  0,  1,  1,  1,      // normal-write bits
+};
+
+uint8_t w_clear_tbl[8] = {
+// v |  0 | 0 | 0 | 0 | 1 | 1 | 1 | 1  |    (value to be written)
+// r |  0 | 0 | 1 | 1 | 0 | 0 | 1 | 1  |    (register value)
+// m |  0 | 1 | 0 | 1 | 0 | 1 | 0 | 1  |    (mask)
+        0,  0,  1,  1,  0,  0,  1,  0,      // normal-write bits
+};
+
 uint8_t w0[8] = {
 // v |  0 | 0 | 0 | 0 | 1 | 1 | 1 | 1  |
 // r |  0 | 0 | 1 | 1 | 0 | 0 | 1 | 1  |
@@ -68,20 +82,34 @@ void test_tables(void)
 				uint8_t index = (v<<2) | (r<<1) | (m);
 				
 				uint8_t w1  = w[index];
+				uint8_t w_set1= w_set_tbl[index];
+				uint8_t w_clear1= w_clear_tbl[index];
 				uint8_t w01 = w0[index];
 				uint8_t rt1 = t[index];
 				uint8_t rts1 = t_set[index];
 				uint8_t rtc1 = t_clear[index];
 				
 				uint8_t w2  = r;
+				uint8_t w_set2= r;
+				uint8_t w_clear2= r;
 				uint8_t w02 = r;
 				uint8_t rt2 = r;
 				uint8_t rts2 = r;
 				uint8_t rtc2 = r;
 				if (m == 1) {
-					 // write
+					 // write writable
 					w2 = v;
 					
+					 // set writable
+					if (v == 1) {
+						w_set2 = 1;
+					}
+
+					 // clear writable
+					if (v == 1) {
+						w_clear2 = 0;
+					}
+
 					 // write-0
 					if (v == 0) {
 						w02 = 0;
@@ -97,6 +125,8 @@ void test_tables(void)
 					rtc2 = (v == 1) ? 0 : rtc2;
 				}
 				TEST_ASSERT_EQUAL_UINT8(w1 , w2);
+				TEST_ASSERT_EQUAL_UINT8(w_set1 , w_set2);
+				TEST_ASSERT_EQUAL_UINT8(w_clear1 , w_clear2);
 				TEST_ASSERT_EQUAL_UINT8(w01, w02);
 				TEST_ASSERT_EQUAL_UINT8(rt1, rt2);				
 				TEST_ASSERT_EQUAL_UINT8(rts1, rts2);				
@@ -111,6 +141,20 @@ uint32_t w_write(uint32_t value, uint32_t rw_register, uint32_t rw_mask)
 {
 	rw_register = rw_register & (~rw_mask);
 	rw_register = rw_register | (value & rw_mask);
+	return rw_register;
+}
+
+uint32_t w_set(uint32_t value, uint32_t rw_register, uint32_t rw_mask)
+{
+	value = value & rw_mask;
+	rw_register = rw_register | value;
+	return rw_register;
+}
+
+uint32_t w_clear(uint32_t value, uint32_t rw_register, uint32_t rw_mask)
+{
+	value = value & rw_mask;
+	rw_register = rw_register & (~value);
 	return rw_register;
 }
 
@@ -178,6 +222,48 @@ void test_w(void)
 		// index = 0b00000vrm -> 0b0111 -> 0x07
 		size_t index = ((val & 0x01) << 2) | ((reg & 0x01) << 1) | (w_mask & 0x01);
 		TEST_ASSERT_EQUAL_UINT8(w[index], result & 0x01);
+		result >>= 1;
+		val >>= 1;
+		reg >>= 1;
+		w_mask >>= 1;
+	}
+}
+
+void test_w_set(void)
+{
+	// bits of val, reg and mask here repeat values in the 'rw', 'w0' and 't' tables
+	uint32_t val    = 0b00001111;
+	uint32_t reg    = 0b00110011;
+	uint32_t w_mask = 0b01010101;
+
+	uint32_t result = w_set(val, reg, w_mask);
+
+	for (size_t i = 0; i < sizeof(reg); i++)
+	{
+		// index = 0b00000vrm -> 0b0111 -> 0x07
+		size_t index = ((val & 0x01) << 2) | ((reg & 0x01) << 1) | (w_mask & 0x01);
+		TEST_ASSERT_EQUAL_UINT8(w_set_tbl[index], result & 0x01);
+		result >>= 1;
+		val >>= 1;
+		reg >>= 1;
+		w_mask >>= 1;
+	}
+}
+
+void test_w_clear(void)
+{
+	// bits of val, reg and mask here repeat values in the 'rw', 'w0' and 't' tables
+	uint32_t val    = 0b00001111;
+	uint32_t reg    = 0b00110011;
+	uint32_t w_mask = 0b01010101;
+
+	uint32_t result = w_clear(val, reg, w_mask);
+
+	for (size_t i = 0; i < sizeof(reg); i++)
+	{
+		// index = 0b00000vrm -> 0b0111 -> 0x07
+		size_t index = ((val & 0x01) << 2) | ((reg & 0x01) << 1) | (w_mask & 0x01);
+		TEST_ASSERT_EQUAL_UINT8(w_clear_tbl[index], result & 0x01);
 		result >>= 1;
 		val >>= 1;
 		reg >>= 1;
