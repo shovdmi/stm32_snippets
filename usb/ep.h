ef3d21a9d2e2961df027e118703064a68f08d9e6
shovdmi@yandex.ru <shovdmi@yandex.ru>
2020-09-10 11:28:24 +0300

    Added W-, W0-, T- and R- masks for USB_EPnR register

--- usb/ep.h
+++ usb/ep.h
@@ -1,12 +1,29 @@
 #ifndef EP_H
 #define EP_H
 
-#define USB_W_MASK  0x0011001100220022
-#define USB_W0_MASK 0x0011001100220022
-#define USB_T_MASK  0x0011001100220022
+/*
+5432 1098 7654 3210
+0111 0000 0111 0000 -- t      0x7070
+1000 0000 1000 0000 -- rc_w0  0x8080
+0000 0111 0000 1111 -- rw     0x070F
+0000 1000 0000 0000 -- r      0x0800
+*/
+
+#define USB_W_MASK  0x070F
+#define USB_W0_MASK 0x8080
+#define USB_T_MASK  0x7070
+#define USB_R_MASK  0x0800
 
 #if ((USB_W_MASK & USB_W0_MASK) != 0) || ((USB_W_MASK & USB_T_MASK) != 0) || ((USB_T_MASK & USB_W0_MASK) != 0)
 #error "Masks have overlapping bits!"
+#endif
+
+#if ((USB_W_MASK & USB_R_MASK) != 0) || ((USB_W0_MASK & USB_R_MASK) != 0) || ((USB_T_MASK & USB_R_MASK) != 0)
+#error "Masks and Read-only mask have overlapping bits!"
+#endif
+
+#if ((USB_W_MASK | USB_W0_MASK | USB_T_MASK | USB_R_MASK) != 0xFFFF)
+#error "Mask's bits missing!"
 #endif
 
 #define EP_SET_BITS(EP_VALUE, VALUE_)     set_bits(VALUE_, EP_VALUE, USB_W_MASK, USB_W0_MASK, USB_T_MASK)
