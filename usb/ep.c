23db0347529390d13c578814712c0f19acb5f9ca
shovdmi <shovdmi@yandex.ru>
2020-11-06 13:55:32 +0300

    first attempt

--- usb/ep.c
+++ usb/ep.c
@@ -1,3 +1,4 @@
+#include "ep.h"
 
 enum usb_stat_t {
 	STAT_DISABLED = 0,
@@ -13,6 +14,18 @@ enum usb_ep_type_kind_t {
 	EP_TYPE_CONTROL_STATUS_OUT = 3, // (1 << 1) | 1,
 	EP_TYPE_ISO =                4, // (2 << 1) | 0,
 	EP_TYPE_INTERRUPT =          6, // (3 << 1) | 0,
+};
+
+
+int init_endpoint(void)
+{
+	uint16_t ep = USB->EP0R;
+	ep_write_ep_type_kind(ep, EP_TYPE_CONTROL);
+	USB->EP0R = ep;
+
+	__asm__("bkpt");
+
+	return 0;
 }
 
 /*
