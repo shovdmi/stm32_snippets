0037052148e1eaf82e209653bd619e2b86041b6e
Дмитрий Шовкун <d.shovkun@atol.ru>
2022-04-06 17:12:41 +0300

    Removed unnecessary comments (moved to the Joplin Notebook [bit_ops.h](:/6bfee0f41b334992b47d027522aa9b81) )

--- usb/bit_ops.h
+++ usb/bit_ops.h
@@ -4,26 +4,11 @@
 #include <stdint.h>
 
 /** \breif
- *
- * R    | 0 | 0 | 0 | 0 | 1 | 1 | 1 | 1 |  0 | 0 | 0 | 0 | 1 | 1 | 1 | 1 |  A
- * V    | 0 | 0 | 1 | 1 | 0 | 0 | 1 | 1 |  0 | 0 | 1 | 1 | 0 | 0 | 1 | 1 |  B
- * WM   | 0 | 1 | 0 | 1 | 0 | 1 | 0 | 1 |  0 | 1 | 0 | 1 | 0 | 1 | 0 | 1 |  C
- * VM   | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |  1 | 1 | 1 | 1 | 1 | 1 | 1 | 1 |  D
- * -----|---|---|---|---|---|---|---|---|----|---|---|---|---|---|---|---|
- * TBW  | 0 | 0 | 0 | 0 | 1 | 1 | 1 | 1 |  0 | 0 | 0 | 1 | 1 | 0 | 1 | 1 |  Y
- *
- * A: rw_register; B: value; C: rw_mask; D: v_mask;
- *
- * y = (A + C) (A + D) (B + C' + D')
- * http://www.32x8.com/pos4_____A-B-C-D_____m_7-8-9-10-12-13-14-15___________option-2_____899788975271824292797
- *
- * y = AC' + AD' + BCD // ?? == A(C'+D') + BCD = A((CD)') + BCD = (A' + CD)' + BCD
- * http://www.32x8.com/sop4_____A-B-C-D_____m_7-8-9-10-12-13-14-15___________option-0_____899788975271824292797
- *
- *
+ * Here we assume the register has no toggleble bits. If it has then tbw-value must be bitwise AND-ed with (~t_mask).
+ * [bit_ops.h](:/6bfee0f41b334992b47d027522aa9b81)
  * @param value
- * @param rw_register
- * @param rw_mask
+ * @param rw_register - register current value
+ * @param rw_mask - (register writeble bit mask / rw_mask) : 1 - read/write bit; 0 - keep (write back) bit current value
  * @return
  */
 static __inline__ uint32_t w_write_bits(uint32_t value, uint32_t v_mask, uint32_t rw_register, uint32_t rw_mask)
@@ -54,29 +39,13 @@ static __inline__ uint32_t w_toggle_bits(uint32_t value, uint32_t rw_register, u
 	return rw_register;
 }
 
-/** \brief
- * R - register value; V - expected register value; tbw- to be written
- *
- * | R | V || tbw | | R op V |
- * |---|---||-----| |--------|
- * | 0 | 0 ||  0  | | = 0&0  |
- * | 0 | 1 ||  0  | | = 0&1  | writing 1's keeps R
- * | 1 | 0 ||  0  | | = 1&0  |
- * | 1 | 1 ||  1  | | = 1&1  | writing 1's keeps R
- *
- * R    | 0 | 0 | 0 | 0 | 1 | 1 | 1 | 1 |  0 | 0 | 0 | 0 | 1 | 1 | 1 | 1 |  A
- * V    | 0 | 0 | 1 | 1 | 0 | 0 | 1 | 1 |  0 | 0 | 1 | 1 | 0 | 0 | 1 | 1 |  B
- * W0M  | 0 | 1 | 0 | 1 | 0 | 1 | 0 | 1 |  0 | 1 | 0 | 1 | 0 | 1 | 0 | 1 |  C
- * VM   | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |  1 | 1 | 1 | 1 | 1 | 1 | 1 | 1 |  D
- * -----|---|---|---|---|---|---|---|---|----|---|---|---|---|---|---|---|
- * TBW  | 0 | x | 0 | x | 1 | 1 | 1 | 1 |  0 | x | 0 | x | 1 | 0 | 1 | 1 |  Y
- *
- * A: rw_register; B: value; C: w0_mask; D: v_mask;
- *
- * y = AC' + AD' + AB
- * y = (A) (B + C' + D')
- *
- *http://www.32x8.com/pos4_____A-B-C-D_____m_8-9-10-12-13-14-15_____d_2-3-6-7_____option-2_____889788965478820592664
+/** \brief 
+ * Here we assume the register has no toggleble bits. If it has then tbw-value must be bitwise AND-ed with (~t_mask).
+ * [bit_ops.h](:/6bfee0f41b334992b47d027522aa9b81)
+ * @param value
+ * @param w0_register - register current value
+ * @param w0_mask - (register write-zero bit mask / w0_mask) : 1 - write-0 to reset bit; 0 - keep (write back) bit current value
+ * @return
  */
 static __inline__ uint32_t w0_write_bits(uint32_t value, uint32_t v_mask, uint32_t w0_register, uint32_t w0_mask)
 {
@@ -85,29 +54,6 @@ static __inline__ uint32_t w0_write_bits(uint32_t value, uint32_t v_mask, uint32
 }
 
 /** \brief
- * R - register value; V - expected register value; tbw- to be written
- *
- * | R | V || tbw | | R op V |
- * |---|---||-----| |--------|
- * | 0 | 0 ||  0  | | = 0^0  | writing 0's keeps R
- * | 0 | 1 ||  1  | | = 0^1  |
- * | 1 | 0 ||  1  | | = 1^0  |
- * | 1 | 1 ||  0  | | = 1^1  | writing 0's keeps R
- *
- * R    | 0 | 0 | 0 | 0 | 1 | 1 | 1 | 1 |  0 | 0 | 0 | 0 | 1 | 1 | 1 | 1  |  A
- * V    | 0 | 0 | 1 | 1 | 0 | 0 | 1 | 1 |  0 | 0 | 1 | 1 | 0 | 0 | 1 | 1  |  B
- * TM   | 0 | 1 | 0 | 1 | 0 | 1 | 0 | 1 |  0 | 1 | 0 | 1 | 0 | 1 | 0 | 1  |  C
- * VM   | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |  1 | 1 | 1 | 1 | 1 | 1 | 1 | 1  |  D
- * -----|---|---|---|---|---|---|---|---|----|---|---|---|---|---|---|----|
- * TBW  | 0 | 0 | 0 | 0 | 1 | 0 | 1 | 0 |  0 | 0 | 0 | 1~| 1 | 1~| 1 | 0  |  Y
- *        r       r       r       r        r       r       r       r
- *
- * A: rw_register; B: value; C: t_mask; D: v_mask;
- *
- * y = y = AC' + AB'D + A'BCD
- * y = y = (A + B) (A + C) (C' + D) (A' + B' + C')
- *
- * http://www.32x8.com/sop4_____A-B-C-D_____m_7-8-10-11-12-13___________option-0_____898788876971825592647
  */
 static __inline__ uint32_t t_write_bits(uint32_t value, uint32_t v_mask, uint32_t t_register, uint32_t t_mask)
 {
@@ -117,16 +63,9 @@ static __inline__ uint32_t t_write_bits(uint32_t value, uint32_t v_mask, uint32_
 
 static __inline__ uint32_t t_set_bits(uint32_t value, uint32_t t_register, uint32_t t_mask)
 {
-	//                (r ^ v) ^ r -> r
-	// r = 1, v = 1 : (1 ^ 1) ^ 1  = 1   // v=1, r set
-	// r = 0, v = 1 : (0 ^ 1) ^ 0  = 1   // v=1, r set
-	// r = 1, v = 0 : (1 ^ 0) ^ 1  = 0   // v=0, r not changed
-	// r = 0, v = 0 : (0 ^ 0) ^ 0  = 0   // v=0, r not changed
+	// see [bit_ops.h](:/6bfee0f41b334992b47d027522aa9b81)
 	//printf("value= 0x%08X, t_register=0x%08X\n",value, x_value);
 	value = value & t_mask;
-	// (reg OR val) is a required value of 'reg' after hw-XOR-ing a looked for number
-	// i.e. we need to make a preceding XOR to get a number which will be XOR-ed with 'reg' by hardware
-	// (t_register | value) ^ t_register  ^(by hw)  t_register  == (t_register | value)
 	uint32_t x_value = (t_register | value) ^ (t_register & t_mask);
 	//printf("t_register | value =0x%08X, t_register & t_mask = 0x%08X, x_value = 0x%08X\n", (t_register | value), (t_register & t_mask), x_value);
 	return x_value;
@@ -134,18 +73,7 @@ static __inline__ uint32_t t_set_bits(uint32_t value, uint32_t t_register, uint3
 
 static __inline__ uint32_t t_clear_bits(uint32_t value, uint32_t t_register, uint32_t t_mask)
 {
-	//                (r & v) ^ r -> r
-	// r = 1, v = 0 : (1 & 0) ^ 1  = 1   // v=1, r not changed
-	// r = 0, v = 0 : (0 & 0) ^ 0  = 1   // v=1, r not changed
-	// r = 1, v = 1 : (1 & 1) ^ 1  = 0   // v=0, r cleared
-	// r = 0, v = 1 : (0 & 1) ^ 0  = 0   // v=0, r cleared
-
-	// if m == 0 and either v == 0 or v == 1, we don't touch r
-	// if m == 1 and v == 1, we reset r.
-	// For example we want to reset bits using value=0b1010 and mask=0b1100. That means
-	// our masked 'value' is going to be 0b1000.
-	// For clearing a bit we use bitwise-AND between r and 1 (one) preceding hardware bitwise-XOR,
-	// i.e (r & 1) == r ----> (this xor is made by usb-core) r ^ r = 0
+	// see [bit_ops.h](:/6bfee0f41b334992b47d027522aa9b81)
 	value = value & t_mask;
 	uint32_t x_value = (t_register & value) | (t_register & (~t_mask));
 	return x_value;
